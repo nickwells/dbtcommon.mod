e74d2cbd920aa2f58037f4dd0e5a525d32361609
Nick Wells <wells.nicholas@gmail.com>
2020-04-26 00:10:08 +0100

    Rename SqlCommand to SQLCommand to keep golint happy

--- dbtcommon/sql_command.go
+++ dbtcommon/sql_command.go
@@ -2,8 +2,8 @@ package dbtcommon
 
 import "os/exec"
 
-// SqlCommand returns the command to be run
-func SqlCommand(fileName string) *exec.Cmd {
+// SQLCommand returns the command to be run
+func SQLCommand(fileName string) *exec.Cmd {
 	return exec.Command(PsqlPath,
 		"-v", "ON_ERROR_STOP=1",
 		"-q",
